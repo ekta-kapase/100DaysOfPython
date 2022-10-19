a0b8327c0eef820edd6b6ab2e42147b1a2942c9e
ektakapase <106029464+ektakapase@users.noreply.github.com>
2022-10-19 18:00:15 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,15 +3,11 @@
 print("Let's play FizzBuzz!")
 
 for number in range(1,101):
-  if number % 3 == 0:
-    if number % 3 ==0 and number % 5 ==0:
+  if number % 3 ==0 and number % 5 ==0:
       print("FizzBuzz")
-    else:
+  elif number % 3 ==0:
       print("Fizz")
   elif number % 5 ==0:
-    if number % 3 ==0 and number % 5 ==0:
-     print("FizzBuzz")
-    else:
       print("Buzz")
   else:
     print(number)
