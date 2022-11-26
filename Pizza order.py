4393cf55ec3d5e09726e01684f09bd0e4c944cd8
Ekta Kapase <106029464+ekta-kapase@users.noreply.github.com>
2022-11-26 15:22:06 +0530

    Update Pizza order.py

--- Pizza order.py
+++ Pizza order.py
@@ -1,32 +1,32 @@
 #Code by Ekta Kapase
 
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N ").lower()
 
 bill = 0;
 
 #size
 
-if size == "S":
+if size == "s":
   bill = 15
-elif size== "M":
+elif size== "m":
   bill = 20
-elif size== "L":
+elif size== "l":
   bill = 25
   
 #pepperoni
   
-if add_pepperoni == "Y":
-  if size=="S":
+if add_pepperoni == "y":
+  if size=="s":
     bill +=2
   else:
     bill+= 3
 
 #cheese
   
-if extra_cheese == "Y":
+if extra_cheese == "y":
   bill +=1
   
 #final_bill
